1b0063b6df8f15f70b001a886b1d7b7c70225978
daichangxin <daichangxin@gmail.com>
2021-12-28 17:17:37 +0800

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable import/extensions */
+// 考虑会魔改laya底层，所以这里引入固定js文件，没事不会升级
+// import外部类库的顺序不可更改
+import '../libs/layabox/laya.core.js';
+import '../libs/layabox/laya.html.js';
+import '../libs/layabox/laya.ani.js';
